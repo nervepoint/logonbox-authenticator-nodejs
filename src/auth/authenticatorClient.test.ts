7e15cfddb9de33e347e734ccde1fc612a77cb17c
garrow <gaurav@javassh.com>
2022-02-23 09:21:12 +0000

    Removed es lint errors

--- src/auth/authenticatorClient.test.ts
+++ src/auth/authenticatorClient.test.ts
@@ -13,7 +13,7 @@ const sshPublicKeyFormat = (principal: string) => {
             ${sshPublicKey.toString("ssh")}`;
 }
 
-const MockedRemoteService = jest.fn<RemoteService, any[]>(() => ({
+const MockedRemoteService = jest.fn(() => ({
     hostname(): string {
         return "some.directory.org";
     },
@@ -26,12 +26,15 @@ const MockedRemoteService = jest.fn<RemoteService, any[]>(() => ({
         return sshPublicKeyFormat(principal);
     },
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     getSignUrl(encodedPayload: string): string {
         return "";
     },
 
     async signPayload(principal: string, remoteName: string, fingerprint: string, text: string,
+                      // eslint-disable-next-line @typescript-eslint/no-unused-vars
                       buttonText: string, encodedPayload: string, flags: number): Promise<SignatureResponse> {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const signer = sshPrivateKey.createSign("sha512" as any);
         signer.update(Buffer.from(encodedPayload, "base64url"));
         const signature = signer.sign();
@@ -72,7 +75,7 @@ describe("authenticator client tests", () => {
     });
 
     it("should throw error if remote response does not return signature", async () => {
-        const MockedFailedRemoteService = jest.fn<RemoteService, any[]>(() => ({
+        const MockedFailedRemoteService = jest.fn(() => ({
             hostname(): string {
                 return "some.directory.org";
             },
@@ -83,7 +86,9 @@ describe("authenticator client tests", () => {
             async keys(principal: string): Promise<string> {
                 return sshPublicKeyFormat(principal);
             },
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             async signPayload(principal: string, remoteName: string, fingerprint: string,
+                              // eslint-disable-next-line @typescript-eslint/no-unused-vars
                         text: string, buttonText: string, encodedPayload: string, flags: number): Promise<SignatureResponse> {
 
                 return {
